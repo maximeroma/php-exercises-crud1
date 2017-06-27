c1b9f1ca3ced18e4a60ac5975f0912244144b50a
romamaxime <roma.maxime1994@gmail.com>
2017-06-27 16:37:23 +0200

    add ex1

---
+++ ex1.php
@@ -0,0 +1,21 @@
+<?php
+try {
+  $bdd = new PDO ('mysql:host=localhost;dbname=colyseum;charset=utf8', 'roma', '8PJFIm!');
+
+}
+catch (Exception $e)
+{
+    die('Erreur : ' . $e->getMessage());
+}
+$reponse = $bdd->query('SELECT * FROM clients');
+
+
+?>
+
+<ul>
+  <?php
+  while($donnees=$reponse->fetch()){
+    echo '<li>'. $donnees['firstName'] . '</li>';
+  }
+  ?>
+</ul>
