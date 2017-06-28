c93e47434a7667deb60880c9e5509c5d95616816
romamaxime <roma.maxime1994@gmail.com>
2017-06-28 10:03:01 +0200

    add ex5

---
+++ ex5.php
@@ -0,0 +1,20 @@
+<?php
+try {
+  $bdd = new PDO ('mysql:host=localhost;dbname=colyseum;charset=utf8', 'roma', '8PJFIm!');
+
+}
+catch (Exception $e)
+{
+    die('Erreur : ' . $e->getMessage());
+}
+$reponse = $bdd->query("SELECT * FROM clients WHERE lastName LIKE 'm%' OR firstName LIKE 'm%'" );
+
+?>
+
+<ul>
+  <?php
+  while($donnees=$reponse->fetch()){
+    echo '<li>'. $donnees['firstName'] ." ".$donnees['lastName']. '</li>';
+  }
+  ?>
+</ul>
